5d3f0f434703bfd38c9b7ad25755cb90da737679
thalassa_home <blinov.gosha@mail.ru>
2023-07-07 18:19:27 +0300

    Initial commit

---
+++ src/main/java/rest/app/WeatherSensorRestApiApplication.java
@@ -0,0 +1,13 @@
+package rest.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherSensorRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherSensorRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rest/app/WeatherSensorRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package rest.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherSensorRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
