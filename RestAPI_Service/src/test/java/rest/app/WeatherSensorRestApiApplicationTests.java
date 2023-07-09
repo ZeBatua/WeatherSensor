7afb558336a20808b5d72ef9ca6e7553f6986492
thalassa_home <blinov.gosha@mail.ru>
2023-07-09 23:30:26 +0300

    Splitting the project into 2 modules

--- RestAPI_Service/src/test/java/rest/app/WeatherSensorRestApiApplicationTests.java
+++ RestAPI_Service/src/test/java/rest/app/WeatherSensorRestApiApplicationTests.java
@@ -1,4 +1,4 @@
-package rest.app;
+package test.java.rest.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
---
+++ SensorClient/src/main/java/sensor/client/App.java
@@ -0,0 +1,10 @@
+package sensor.client;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!!!" );
+    }
+}
