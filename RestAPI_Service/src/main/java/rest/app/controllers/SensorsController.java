293046396002f6fd2bcc96f45ed99fc286652fa6
thalassa_home <blinov.gosha@mail.ru>
2023-07-09 23:26:11 +0300

    init controllers

---
+++ RestAPI_Service/src/main/java/rest/app/controllers/SensorsController.java
@@ -0,0 +1,11 @@
+package rest.app.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/sensors")
+public class SensorsController {
+}
