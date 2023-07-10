3bd89619f2d1bb0356e4f8821c3edc6e28fba620
thalassa_home <blinov.gosha@mail.ru>
2023-07-10 13:52:14 +0300

    register method in Controller

--- RestAPI_Service/src/main/java/rest/app/controllers/SensorsController.java
+++ RestAPI_Service/src/main/java/rest/app/controllers/SensorsController.java
@@ -1,11 +1,30 @@
 package rest.app.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import rest.app.models.Sensor;
+import rest.app.services.SensorService;
 
 @RestController
 @RequestMapping("/sensors")
 public class SensorsController {
+
+    private final SensorService sensorService;
+
+    @Autowired
+    public SensorsController(SensorService sensorService) {
+        this.sensorService = sensorService;
+    }
+
+    @PostMapping("/registration")
+    public ResponseEntity<HttpStatus> registration(@RequestBody Sensor sensor) {
+        sensorService.register(sensor);
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
 }
