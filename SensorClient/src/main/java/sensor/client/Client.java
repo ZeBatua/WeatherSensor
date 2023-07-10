d7158d9deb85796ba34e5c7d549f599aedabd5d8
thalassa_home <blinov.gosha@mail.ru>
2023-07-10 16:18:36 +0300

    change names

--- SensorClient/src/main/java/sensor/client/Client.java
+++ SensorClient/src/main/java/sensor/client/Client.java
@@ -12,7 +12,7 @@ import java.util.Random;
 
 public class Client {
     public static void main(String[] args) {
-        final String sensor = "TestSensor_1";
+        final String sensor = "TestSensor_6";
 
         registerSensor(sensor);
         getMeasurement(sensor);
@@ -32,19 +32,19 @@ public class Client {
 
         double minTemperature = -55.0;
         double maxTemperature = 125.0;
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 1; i++) {
             System.out.println(i);
             sendMeasurement(random.doubles(-55.0, 125).iterator().nextDouble(),
                     random.nextBoolean(), sensor);
         }
     }
 
-    private static void sendMeasurement(double value, boolean raining, String sensorName) {
+    private static void sendMeasurement(double temperature, boolean rainStatus, String sensorName) {
         final String url = "http://localhost:8080/measurements/add";
 
         Map<String, Object> jsonData = new HashMap<>();
-        jsonData.put("value", value);
-        jsonData.put("raining", raining);
+        jsonData.put("temperature", temperature);
+        jsonData.put("rainStatus", rainStatus);
         jsonData.put("sensor", Map.of("name", sensorName));
 
         makePostRequestWithJSONSensorData(url, jsonData);
@@ -57,6 +57,7 @@ public class Client {
         headers.setContentType(MediaType.APPLICATION_JSON);
 
         HttpEntity<Object> request = new HttpEntity<>(jsonData, headers);
+        System.out.println("jsonData = " + jsonData);
 
         try {
             restTemplate.postForObject(url, request, String.class);
