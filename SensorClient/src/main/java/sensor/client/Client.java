e051c7ef8d40812e283f18f8a532a339e94084ba
thalassa_home <blinov.gosha@mail.ru>
2023-07-10 14:01:07 +0300

    new main

---
+++ SensorClient/src/main/java/sensor/client/Client.java
@@ -0,0 +1,11 @@
+package sensor.client;
+
+public class Client {
+    public static void main(String[] args) {
+        final String sensor = "TestSensor_1";
+
+        registerSensor(sensor);  // TODO
+        sendMeasurement(sensor);  // TODO
+
+    }
+}
