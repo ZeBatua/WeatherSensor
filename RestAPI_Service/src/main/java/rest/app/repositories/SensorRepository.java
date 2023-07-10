4ca35653b72a3a5826e9f08a52368beb6ff9a969
thalassa_home <blinov.gosha@mail.ru>
2023-07-10 13:51:15 +0300

    fix wrong signature

--- RestAPI_Service/src/main/java/rest/app/repositories/SensorRepository.java
+++ RestAPI_Service/src/main/java/rest/app/repositories/SensorRepository.java
@@ -2,8 +2,8 @@ package rest.app.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import rest.app.models.Measurement;
+import rest.app.models.Sensor;
 
 @Repository
-public interface SensorRepository extends JpaRepository<Measurement, Integer> {
+public interface SensorRepository extends JpaRepository<Sensor, Integer> {
 }
