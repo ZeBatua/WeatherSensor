f818108455523adba3c4120cdc69f25904d4bf72
thalassa_home <blinov.gosha@mail.ru>
2023-07-10 14:46:54 +0300

    controller add method

--- RestAPI_Service/src/main/java/rest/app/controllers/MeasurementController.java
+++ RestAPI_Service/src/main/java/rest/app/controllers/MeasurementController.java
@@ -1,9 +1,30 @@
 package rest.app.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import rest.app.models.Measurement;
+import rest.app.services.MeasurementService;
 
 @RestController
 @RequestMapping("/measurements")
 public class MeasurementController {
+
+    MeasurementService measurementService;
+
+    @Autowired
+    public MeasurementController(MeasurementService measurementService) {
+        this.measurementService = measurementService;
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<HttpStatus> add(@RequestBody Measurement measurement) {
+        measurementService.addMeasurement(measurement);  // TODO
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
 }
