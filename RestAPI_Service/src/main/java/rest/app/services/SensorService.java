1d75d1b598cb6cd5bf6b924e4d128a28900c4a91
thalassa_home <blinov.gosha@mail.ru>
2023-07-10 13:52:03 +0300

    register method in Service

--- RestAPI_Service/src/main/java/rest/app/services/SensorService.java
+++ RestAPI_Service/src/main/java/rest/app/services/SensorService.java
@@ -1,9 +1,24 @@
 package rest.app.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import rest.app.models.Sensor;
+import rest.app.repositories.SensorRepository;
 
 @Service
 @Transactional(readOnly = true)
 public class SensorService {
+
+    private final SensorRepository sensorRepository;
+
+    @Autowired
+    public SensorService(SensorRepository sensorRepository) {
+        this.sensorRepository = sensorRepository;
+    }
+
+    @Transactional
+    public void register(Sensor sensor) {
+        sensorRepository.save(sensor);
+    }
 }
