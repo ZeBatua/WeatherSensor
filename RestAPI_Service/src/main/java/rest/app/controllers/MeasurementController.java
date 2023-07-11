d2d99c0fb5d8cc633605d602ff8ac735a76ac660
thalassa_home <blinov.gosha@mail.ru>
2023-07-11 19:41:58 +0300

    add validation & exception handler method

--- RestAPI_Service/src/main/java/rest/app/controllers/MeasurementController.java
+++ RestAPI_Service/src/main/java/rest/app/controllers/MeasurementController.java
@@ -1,16 +1,23 @@
 package rest.app.controllers;
 
+import jakarta.validation.Valid;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import rest.app.dto.MeasurementDTO;
 import rest.app.dto.MeasurementsResponse;
 import rest.app.models.Measurement;
 import rest.app.services.MeasurementService;
+import rest.app.util.MeasurementErrorResponse;
+import rest.app.util.MeasurementException;
+import rest.app.util.MeasurementValidator;
 
 import java.util.stream.Collectors;
+
+import static rest.app.util.ErrorsUtil.returnErrorsToClient;
 
 @RestController
 @RequestMapping("/measurements")
@@ -18,16 +25,25 @@ public class MeasurementController {
 
     MeasurementService measurementService;
     ModelMapper modelMapper;
+    MeasurementValidator measurementValidator;
 
     @Autowired
-    public MeasurementController(MeasurementService measurementService, ModelMapper modelMapper) {
+    public MeasurementController(MeasurementService measurementService, ModelMapper modelMapper, MeasurementValidator measurementValidator) {
         this.measurementService = measurementService;
         this.modelMapper = modelMapper;
+        this.measurementValidator = measurementValidator;
     }
 
     @PostMapping("/add")
-    public ResponseEntity<HttpStatus> add(@RequestBody Measurement measurement) {
-        measurementService.addMeasurement(measurement);
+    public ResponseEntity<HttpStatus> add(@RequestBody @Valid MeasurementDTO measurementDTO,
+                                          BindingResult bindingResult) {
+        Measurement measurementToAdd = convertToMeasurement(measurementDTO);
+
+        measurementValidator.validate(measurementToAdd, bindingResult);
+        if (bindingResult.hasErrors())
+            returnErrorsToClient(bindingResult);
+
+        measurementService.addMeasurement(measurementToAdd);
         return ResponseEntity.ok(HttpStatus.OK);
     }
 
@@ -43,6 +59,16 @@ public class MeasurementController {
 
     private MeasurementDTO convertToMeasurementDTO(Measurement measurement) {
         return modelMapper.map(measurement, MeasurementDTO.class);
+    }
+
+    @ExceptionHandler
+    private ResponseEntity<MeasurementErrorResponse> handleException(MeasurementException e) {
+        MeasurementErrorResponse response = new MeasurementErrorResponse(
+                e.getMessage(),
+                System.currentTimeMillis()
+        );
+
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
 }
