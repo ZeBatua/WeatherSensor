da0d014dced9b7ecda38f6b86e5a213355cd1011
thalassa_home <blinov.gosha@mail.ru>
2023-07-11 18:53:01 +0300

    add model mapper bean

--- RestAPI_Service/src/main/java/rest/app/WeatherSensorRestApiApplication.java
+++ RestAPI_Service/src/main/java/rest/app/WeatherSensorRestApiApplication.java
@@ -1,13 +1,19 @@
 package rest.app;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class WeatherSensorRestApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherSensorRestApiApplication.class, args);
+	}
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
 	}
 
 }
