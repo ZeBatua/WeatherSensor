6896069bc9bd7fae68c13387aad8b78c19cbc3c1
thalassa_home <blinov.gosha@mail.ru>
2023-07-11 18:54:12 +0300

    convertor methods & getMeasurements mapping

--- RestAPI_Service/src/main/java/rest/app/controllers/MeasurementController.java
+++ RestAPI_Service/src/main/java/rest/app/controllers/MeasurementController.java
@@ -1,30 +1,48 @@
 package rest.app.controllers;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import rest.app.dto.MeasurementDTO;
+import rest.app.dto.MeasurementsResponse;
 import rest.app.models.Measurement;
 import rest.app.services.MeasurementService;
+
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/measurements")
 public class MeasurementController {
 
     MeasurementService measurementService;
+    ModelMapper modelMapper;
 
     @Autowired
-    public MeasurementController(MeasurementService measurementService) {
+    public MeasurementController(MeasurementService measurementService, ModelMapper modelMapper) {
         this.measurementService = measurementService;
+        this.modelMapper = modelMapper;
     }
 
     @PostMapping("/add")
     public ResponseEntity<HttpStatus> add(@RequestBody Measurement measurement) {
         measurementService.addMeasurement(measurement);
         return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    @GetMapping()
+    public MeasurementsResponse getMeasurements() {
+        return new MeasurementsResponse(measurementService.findAll().stream().map(this::convertToMeasurementDTO)
+                .collect(Collectors.toList()));
+    }
+
+    private Measurement convertToMeasurement(MeasurementDTO measurementDTO) {
+        return modelMapper.map(measurementDTO, Measurement.class);
+    }
+
+    private MeasurementDTO convertToMeasurementDTO(Measurement measurement) {
+        return modelMapper.map(measurement, MeasurementDTO.class);
     }
 
 }
