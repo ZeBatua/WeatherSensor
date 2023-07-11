0167d1723bf5783d7ca59a68994ca49c3ad25cdd
thalassa_home <blinov.gosha@mail.ru>
2023-07-11 18:22:47 +0300

    init DrawGraph.java

---
+++ SensorClient/src/main/java/sensor/client/DrawGraph.java
@@ -0,0 +1,11 @@
+package sensor.client;
+
+
+import java.util.List;
+
+public class DrawChart {
+    public static void main(String[] args) {
+        List<Double> temparatures = getTemperaturesData(); // TODO
+        drawChart(temparatures); // TODO
+    }
+}
